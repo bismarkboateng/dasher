31ba6496b09d843c540a1d57958b422377ed7213
Bismark <bismarkb609@gmail.com>
2024-03-03 23:01:47 +0000

    update readme, setting up firebase backend

--- components/Search.tsx
+++ components/Search.tsx
@@ -16,16 +16,17 @@ export default function Search() {
 
   return (
     <form className="hidden md:flex md:flex-row md:items-center 
-    bg-[#2C2C2C] pl-1 py-[2px] rounded-md" onSubmit={handleSearch}>
+    bg-[#2C2C2C] pl-1 py-[2px] rounded-md"
+     onSubmit={handleSearch}>
      <div>
-      <IoIosSearch className="text-base text-white font-bold ml-2 mr-2" />
+      <IoIosSearch fontSize={22} className="text-base text-white font-bold ml-2 mr-2" />
      </div>
      
      <input
       value={searchTerm}
       onChange={(event) => setSearchTerm(event.target.value)}
       className="bg-[#2C2C2C] border-none outline-none placeholder:text-sm
-      text-white text-sm"
+      text-white text-sm py-1"
       placeholder="Search"
      />
      <button hidden type="submit"></button>
