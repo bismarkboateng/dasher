77fc17943b84abd830f5524112d750ef7099b716
Bismark <bismarkb609@gmail.com>
2024-03-20 06:33:26 +0000

    add product logic implemented

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -17,11 +17,9 @@ export default  function Products() {
     const getProducts = async () => {
       const querySnapshot = await getDocs(collection(database, "products"));
       const newProducts: Product[] = []
-
       querySnapshot.forEach((doc) => {
         newProducts.push(doc.data())
       });
-
       setProducts(newProducts)
     }
 
@@ -34,22 +32,18 @@ export default  function Products() {
 
   return (
     <section className="relative w-full bg-black h-screen text-white">
-      <section className="absolute top-0 left-0 w-[85%] ml-[20%] h-[40vh] z-999
-       mt-5">
        {isOpen && <AddProductForm />}
-      </section>
-
       <div className="flex flex-row items-center justify-between w-[85%] mx-auto pt-5">
-        <h1 className="text-white text-2xl font-bold mb-5 pt-3 text-center">All Products</h1>
-
+        <h1 className="text-white text-lg font-bold mb-5 pt-3 text-center">All Products</h1>
         <div className="text-white bg-[#141A29] rounded-md outline-none text-xs
-          py-2 px-5 border border-[#2c2c2c] shadow-xl cursor-pointer"
-        onClick={() => handleOpenProductModal()}>Add Product</div>
+          py-2 px-5 border border-[#2c2c2c] shadow-xl cursor-pointer" onClick={() => handleOpenProductModal()}>
+            Add Product
+        </div>
       </div>
       
 
-      <section className="overflow-x-hidden h-[40vh]">
-        <table className="border-collapse overflow-x-scroll text-base ml-3 w-[100%] md:w-[95%] mx-auto h-full">
+      <section className="ml-4">
+        <table className="border-collapse text-base ml-3 w-[100%] md:w-[95%] mx-auto h-full">
           <thead className="mb-2">
             <tr>
               <th className={thStyles}>Name</th>
--- components/AddProductForm.tsx
+++ components/AddProductForm.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { ChangeEvent, FormEvent, useState } from "react";
+import { TailSpin } from "react-loader-spinner"
+import { ToastContainer, toast } from "react-toastify"
+
+
+import { database } from "@/lib/firebase";
+import { addDoc, collection } from "firebase/firestore"
 import { useProductModal } from "@/store/ProductModal";
 import Input from "./Input";
 
 export default function AddProductForm() {
   const handleCloseProductModal = useProductModal(state => state.handleCloseProductModal)
   const [product, setProduct] = useState({} as Product);
+  const [loadingState, setLoadingState] = useState("")
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -15,12 +22,57 @@ export default function AddProductForm() {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    // submit to firebase backend
-    console.log(product)
+
+    const addDocumentToFirebase = async () => {
+      setLoadingState("adding")
+      await addDoc(collection(database, "products"), {
+        ...product,
+        price: Number(product.price),
+        quantity: Number(product.quantity),
+        size: Number(product.size)
+      })
+      setLoadingState("added")
+    }
+
+    addDocumentToFirebase()
+  }
+
+  let buttonState
+
+  if (loadingState == "adding") {
+    buttonState = (
+      <div className="flex flex-row items-center gap-1">
+        <p className="text-white text-xs">adding product</p>
+        <TailSpin
+          visible={true}
+          height="10"
+          width="10"
+          color="#fff"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+     )
+  } else if (loadingState == "added") {
+    toast("Product Added Sucessfully")
+    buttonState = (
+      <div className="text-white text-xs">
+        <p>Added</p>
+        <ToastContainer />
+      </div>
+    )
+  } else {
+    buttonState = (
+      <button className="bg-[#786eff] text-white py-2 px-8 rounded-md
+      text-sm">Submit</button>
+    )
   }
 
   return (
-    <section className="bg-[#141A29] border border-[#2c2c2c] p-3 w-[70%]">
+    <section className="bg-[#141A29] border border-[#2c2c2c] p-3 w-[70%]
+    absolute top-0 left-0 ml-[10%] mt-[10%]">
       <h1 className="text-white text-2xl font-bold">Add Product</h1>
       <hr className="mb-8 mt-8"/>
       <form className="" onSubmit={handleSubmit}>
@@ -105,8 +157,7 @@ export default function AddProductForm() {
 
         <div className="flex flex-row items-center justify-between w-[85%] mx-auto mt-10
         mb-4">
-          <button className="bg-[#786eff] text-white py-2 px-8 rounded-md
-           text-sm" onClick={() => {}}>Submit</button>
+          {buttonState}
           <button className="bg-[#2c2c2c] text-white py-2 px-8 rounded-md
          text-sm" onClick={() => handleCloseProductModal()}>Cancel</button>
         </div>
